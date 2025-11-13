7f70d175ea85624fe01115856109c212f92847c9
KaedeZzz <zhanfengzhou0408@gmail.com>
2025-11-13 13:23:07 +0000

    Metropolis-Hastings

--- MHrank.py
+++ MHrank.py
@@ -6,9 +6,10 @@ from tqdm import tqdm
 
 def MH_sample(games, num_players, num_its):
 
+    # Perform Metropolis-Hastings sampling of player skills given game outcomes.
     # pre-process data:
     # array of games for each player, X[i] = [(other_player, outcome), ...]
-    X = [[] for _ in range(num_players)] 
+    X = [[] for _ in range(num_players)] # Create an empty array
     for a, (i,j) in enumerate(games):
         X[i].append((j, +1))  # player i beat player j
         X[j].append((i, -1))  # player j lost to payer i
@@ -20,6 +21,8 @@ def MH_sample(games, num_players, num_its):
 
     w = np.zeros(num_players)  # skill for each player
 
+    accepted = 0
+    total = 0
     for itr in tqdm(range(num_its)):
         for i in range(num_players):
             j, outcome = X[i].T
@@ -28,12 +31,18 @@ def MH_sample(games, num_players, num_its):
             lp1 = norm.logpdf(w[i]) + np.sum(norm.logcdf(outcome*(w[i]-w[j])))
 
             # proposed new skill and log-prob
-            # TODO
+            w_prop = w[i] + 0.6 * np.random.randn()
+            lp2 = norm.logpdf(w_prop) + np.sum(norm.logcdf(outcome*(w_prop-w[j])))
 
             # accept or reject move:
-            # TODO
+            U = np.random.rand()
+            if np.log(U) < lp2 - lp1:
+                w[i] = w_prop
+                accepted += 1
+            total += 1
 
         skill_samples[:, itr] = w
 
+    print(f'Acceptance rate: {accepted/total:.3f}')
     return skill_samples
 
